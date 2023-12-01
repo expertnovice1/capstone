9d7974ca501acdf307261ba729979f9b29af263b
expertnovice1 <expertnovice1@gmail.com>
2023-12-01 04:06:24 +0000

    first module use - vpc

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,14 @@
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = false
-  routing_mode            = "GLOBAL"
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "8.0.0"
+  # insert the 3 required variables here
+  project_id = "capstone-406620"
+  network_name = "capstone-network-001"
+  subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-central1"
+        },
+        ]
 }
